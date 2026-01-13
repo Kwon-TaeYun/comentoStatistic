db674a8f88d5b772aded8dfa99f9e678637c42e3
Kwon Tae-Yun <taeyun1118@naver.com>
2026-01-13 13:52:40 +0900

    feat:연별, 연월별 로그인 수 조회 구현 완료

--- src/main/java/com/demo/comentoStatistic/dto/YearCountDto.java
+++ src/main/java/com/demo/comentoStatistic/dto/YearCountDto.java
@@ -1,4 +1,21 @@
 package com.demo.comentoStatistic.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class YearCountDto {
+    private String year;
+    private Integer totCnt;
+
+    public String getYear() {
+        return year;
+    }
+
+    public Integer getTotCnt() {
+        return totCnt;
+    }
 }
--- src/main/java/com/demo/comentoStatistic/dto/YearMonthCountDto.java
+++ src/main/java/com/demo/comentoStatistic/dto/YearMonthCountDto.java
@@ -1,4 +1,19 @@
 package com.demo.comentoStatistic.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class YearMonthCountDto {
+    private String yearMonth;
+    private Integer totCnt;
+
+    public String getYear() {
+        return yearMonth;
+    }
+
+    public Integer getTotCnt() {
+        return totCnt;
+    }
 }
